5ef33b81926fdd660b75ea610fcc1a0469eba4b1
teichen <paul.teichen@gmail.com>
2023-05-12 10:50:16 -0700

    sqlalchemy to start sql session

--- data/CreateSQLData.py
+++ data/CreateSQLData.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import configparser
 
 class CreateSQLData:
     __metaclass__ = abc.ABCMeta
@@ -6,4 +8,28 @@ class CreateSQLData:
     def __init__(self):
         """ base class for SQL Data Creation
         """
-        pass
+        config = configparser.ConfigParser()
+        config.read('inputs.ini')
+
+        url = str(config['sql']['address'])
+
+        # start a sql session
+        engine  = create_engine(url, encoding='utf-8', convert_unicode=True, pool_size=30, pool_recycle=3600)
+        session_interface = sessionmaker(bind=engine)
+        session = scoped_session(session_interface)()
+
+        session.commit() # persist permanently to disk
+
+        # create input measurements
+
+        inputs = []
+        inputs.append(single_measurement())
+
+        # add data to sql session
+
+        session.add_all(inputs)
+        session.commit()
+
+        # close sql session
+
+        session.close()
