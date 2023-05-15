7e3bb879f24317181817c4e18abe099bb969e34f
teichen <paul.teichen@gmail.com>
2023-05-15 12:54:16 -0700

    mongo client insert data

--- data/CreateMongoData.py
+++ data/CreateMongoData.py
@@ -16,16 +16,24 @@ class CreateMongoData:
         config.read('inputs.ini')
 
         url = str(config['mongo']['address'])
+        db_name = str(config['mongo']['name'])
+        db_collection = str(config['mongo']['collection'])
 
         # start a mongo session
+        mc = MongoClient(url, uuidRepresentation='pythonLegacy')
+        db = mc[db_name][db_collection]
+
         # create input measurements
 
         input_name  = 'test_name'
         input_value = 1.0
         input_time  = datetime(2005, 6, 1, 0, 0)
 
+        inputs = {'input_name': input_name, 'input_value': input_value, 'input_time': input_time}
 
         # add data to mongo session
+        db.insert_one(inputs)
+        
         # close mongo session
 
 
