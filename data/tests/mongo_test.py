add836d5a6049a8d6d8fdbf04c489bbd94182358
teichen <paul.teichen@gmail.com>
2023-06-26 20:28:08 -0700

    add mongo unittest

---
+++ data/tests/mongo_test.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.insert(1, '../')
+
+import unittest
+from sqlalchemy import text
+from CreateMongoData import CreateMongoData
+from datetime import datetime
+
+config_path = '../inputs.ini'
+
+class MongoTest(unittest.TestCase):
+
+    mongo_db = CreateMongoData(config_path)
+
+    def test_mongo(self):
+        # retrieve data
+        start_time = datetime(2005, 5, 29, 0, 0)
+        stop_time  = datetime(2005, 6, 2, 0, 0)
+        query = {'input_time': {'$gte': start_time, '$lt': stop_time}}
+        cursor = self.mongo_db.db.find(query)
+
+        inputs = {}
+        inputs['test_name'] = {}
+
+        for doc in cursor:
+            input_time = doc['input_time']
+            inputs['test_name'][input_time] = doc['input_value']
+
+        test_dict = {'test_name': {datetime(2005, 6, 1, 0, 0): 1.0}}
+
+        self.assertDictEqual(inputs, test_dict)
+
+if __name__ == "__main__":
+    unittest.main()
+
