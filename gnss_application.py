b039c51e5460752800257029146d3850e3c6779e
teichen <paul.teichen@gmail.com>
2023-12-12 22:33:51 -0800

    FIX dylib ctypes entry point

--- gnss_application.py
+++ gnss_application.py
@@ -1,19 +1,53 @@
 import ctypes
-from ctypes import c_size_t
+from ctypes import c_void_p, c_double, c_size_t, POINTER, pointer
 import os
 import sys
 import numpy as np
+from numpy import random
+from memory_profiler import profile
 
-path = os.path.dirname(__file__)
-multifilter_controller = ctypes.CDLL(os.path.join(path, "multifilter_controller.dylib"))
+# set seed
+random.seed(10)
 
-run = multifilter_controller.run
+INPUT_BUFFER_SIZE = 1000
 
-run.restype = None
-run.argtypes = [np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C"), c_size_t]
+@profile
+def filter_data():
+    """
+    """
+    path = os.path.dirname(__file__)
+    multifilter_controller = ctypes.CDLL(os.path.join(path, "multifilter_controller.dylib"))
 
-inputs = np.ones(100, np.float64)
-res = run(inputs, inputs.size)
+    # dynamic memory allocation handled by dylib
+    allocate_inputs = multifilter_controller.allocate_inputs
+    allocate_inputs.restype = POINTER(c_double * INPUT_BUFFER_SIZE)
 
-print(res)
+    inputs = allocate_inputs()
 
+    run = multifilter_controller.run
+    run.restype  = c_void_p
+    run.argtypes = [POINTER(c_double * INPUT_BUFFER_SIZE)]
+
+    # define inputs {timestamp (float), carrier_id (int), carrier_freq (float), carrier_phase (float)}
+    n_inputs = 10
+    times         = np.linspace(0, n_inputs-1, num=n_inputs).reshape((n_inputs, 1))
+    carrier_id    = 1 * np.ones((n_inputs, 1))
+    carrier_freq  = 1575.42 * np.ones((n_inputs, 1)) # MHz
+    carrier_phase = random.rand(n_inputs).reshape((n_inputs, 1))
+
+    for idx in range(n_inputs):
+        inputs.contents[idx * 4]     = times[idx]
+        inputs.contents[idx * 4 + 1] = carrier_id[idx]
+        inputs.contents[idx * 4 + 2] = carrier_freq[idx]
+        inputs.contents[idx * 4 + 3] = carrier_phase[idx]
+
+    run(inputs)
+
+    free_inputs = multifilter_controller.free_inputs
+    free_inputs.restype  = c_void_p
+    free_inputs.argtypes = [POINTER(c_double * INPUT_BUFFER_SIZE)]
+
+    free_inputs(inputs)
+
+if __name__ == "__main__":
+    filter_data()
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <gperftools/profiler.h>
 
 extern "C" {
 #include "Controller.h"
@@ -7,10 +8,29 @@ extern "C" {
 using namespace std;
 
 extern "C" {
-    void run(double*, size_t n_inputs);
+    double* allocate_inputs(){
+    double* inputs;
+    inputs = (double*) calloc(100, sizeof(double));
+    return inputs;
+    }
 }
 
-extern "C" void run(double* inputs, size_t n_inputs){
+extern "C" {
+    void free_inputs(double* inputs){
+    delete [] inputs;
+    }
+}
+
+extern "C" {
+    void run(double *inputs){
+    // inputs: 4-pairs of
+    //     timestamp (float)
+    //     carrier_id (int)
+    //     carrier_freq (float)
+    //     carrier_phase (float)}
+
+    ProfilerStart("/tmp/prof.out"); // memory profiler
+
     bool logging;
     string model_type;
     int n_filters;
@@ -19,8 +39,23 @@ extern "C" void run(double* inputs, size_t n_inputs){
     model_type = "Harmonic";
     n_filters  = 4;
 
-    // TODO: measurement data subject to corruption noise
-    Controller process_data(logging, model_type, n_filters);
+    // TODO instantiate controller
+    // Controller multifilter_controller(logging, model_type, n_filters);
+
+    // process data (measurement data subject to corruption noise)
+    double t;
+    double input_data_t[3];
+
+    // test first time
+    t = inputs[0];
+    input_data_t[0] = inputs[1];
+    input_data_t[1] = inputs[2];
+    input_data_t[2] = inputs[3];
+
+    // TODO filter data
+    //multifilter_controller.update_filters(t, input_data_t);
+    ProfilerStop();
+    }
 }
 
 int main()
